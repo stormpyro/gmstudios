630f28f9e72e65885d6a4d8a991b760ce8f0c721
Stormpyro <tatto1795@gmail.com>
2022-03-22 13:00:08 -0500

    GM - Add carousel

--- src/.vuepress/components/home.vue
+++ src/.vuepress/components/home.vue
@@ -1,6 +1,23 @@
 <template>
-  <div>
+  <div style="height: 100vh">
     <NavBar />
+    <carousel autoplay style="margin-top: 50px; max-height: 700px">
+      <slide
+        ><img style="width: 100%" :src="require('../public/images/sign.jpg')"
+      /></slide>
+      <slide
+        ><img
+          style="width: 100%"
+          :src="require('../public/images/hammer.jpg')"
+        />
+      </slide>
+      <slide
+        ><img
+          style="width: 100%"
+          :src="require('../public/images/taxes.jpg')"
+        />
+      </slide>
+    </carousel>
   </div>
 </template>
 <script>
--- src/.vuepress/enhanceApp.js
+++ src/.vuepress/enhanceApp.js
@@ -1,6 +1,8 @@
 import Vuesax from "vuesax";
 import "vuesax/dist/vuesax.css";
 import "boxicons/css/boxicons.min.css";
+import VueCarousel from "vue-carousel";
+import { Carousel, Slide } from "vue-carousel";
 
 const requireComponent = require.context(
   "./global-components",
@@ -16,7 +18,11 @@ export default ({
 }) => {
   // ...apply enhancements for the site.
   Vue.use(Vuesax);
+  Vue.use(VueCarousel);
+  Vue.component(Carousel);
+  Vue.component(Slide);
 
+  // Register all global-components
   requireComponent.keys().forEach((fileName) => {
     const componentConfig = requireComponent(fileName);
 
